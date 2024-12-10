45f289390da19eb919acf9c6999fdcea08508cad
yesnake <166038484+yesnake@users.noreply.github.com>
2024-12-10 17:30:16 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+  "name": "My TonConnect App", 
+  "iconUrl": "https://example.com/icon.png",  // Optional: URL to your app's icon
+  "url": "https://example.com",  // Your app's homepage URL
+  "description": "A brief description of your app",
+  "themeColor": "#FFFFFF",  // Optional: theme color for the app's UI
+  "networks": [
+    "testnet", 
+    "mainnet"
+  ],
+  "wallets": [
+    {
+      "name": "tonkeeper",
+      "iconUrl": "https://example.com/tonkeeper-icon.png"
+    },
+    {
+      "name": "tonhub",
+      "iconUrl": "https://example.com/tonhub-icon.png"
+    },
+    {
+      "name": "freeTON",
+      "iconUrl": "https://example.com/freeton-icon.png"
+    }
+  ]
+}
